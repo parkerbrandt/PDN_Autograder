67850dd38f08822fe50545e801bc3394ddefca65
Parker Brandt <parker.brandt101@gmail.com>
2023-02-18 01:50:54 -0600

    Example directory with individual problem python scripts

---
+++ Project_3/Your_Solution_Project_3/autograder_problem_3_2.py
@@ -0,0 +1,19 @@
+import os
+import termcolor
+from autograder_base import autograde
+
+student_files = os.path.join(".", "Problem_1")
+test_files = os.path.join("..", "test_data", "Problem_1")
+
+
+def grade_problem_1():
+
+    
+
+    return
+
+
+if __name__ == "__main__":
+    grade, time = grade_problem_1()
+    print("Grade")
+
---
+++ Project_3/Your_Solution_Project_3/autograder_project_3.py
@@ -0,0 +1,10 @@
+from autograder_base import autograde
+import autograder_problem_3_1 as p1
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
