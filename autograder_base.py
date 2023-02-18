3b2d1d3af13c0adbe277027a7a018ec1d43011bb
Parker Brandt <parker.brandt101@gmail.com>
2023-02-18 10:42:15 -0600

    Changed terminal output coloring to use termcolor colored()

--- autograder_base.py
+++ autograder_base.py
@@ -4,7 +4,7 @@ import filecmp
 import os
 import platform
 import re
-import termcolor
+from termcolor import colored
 
 
 """
@@ -21,13 +21,6 @@ import termcolor
 
 # To get debug messages
 DEBUG = True
-
-# Colors for console printing,
-W = '\033[0m'   # white (normal)
-R = '\033[31m'  # red
-O = '\033[33m'  # orange
-Y = '\033[93m'  # yellow
-G = '\033[32m'  # green
 
 # -----------------------------------------
 # generate file names from a given template
@@ -60,21 +53,21 @@ def grade_problem(student_dir, t_output, t_res, commands, exact):
     try:
         # alert what directory doesn't have a makefile (dont copy it)
         if not os.path.isfile(os.path.join(student_dir, "Makefile")):
-            print(f"\n{R}ERROR: Missing Makefile when trying to test in {student_dir}! {W}")
-            print(f"{R}       Skipping testing for {commands[0][0]}...{W}")
+            print(colored(f"\nERROR: Missing Makefile when trying to test in {student_dir}!", "red"))
+            print(colored(f"       Skipping testing for {commands[0][0]}...", "red"))
             return [scores, times]
 
         # run makefile
         command = f"(cd {student_dir} && make)"
         err = os.system(command)
         if err != 0:
-            print(f"{R}Error making the program {commands[0][0]}{W}")
+            print(colored(f"Error making the program {commands[0][0]}", "red"))
             return [scores, times]
 
         # run and analyze the problem
         for i in range(0, n):
 
-            print(f"{G}Testing for {commands[i][0]}'s output: {t_res[i]}{W}")
+            print(colored(f"Testing for {commands[i][0]}'s output: {t_res[i]}", "green"))
 
             # generate the command and run the program
             #   ex. ./Problem_1/parallel_mult_mat_mat in.csv 10 10 ...
@@ -89,34 +82,34 @@ def grade_problem(student_dir, t_output, t_res, commands, exact):
 
             # skip over if cant run the program...
             if err != 0:
-                print(f"{R}\nError running the program {commands[i][0]}{W}")
+                print(colored(f"\nError running the program {commands[i][0]}", "red"))
                 continue
 
             # get data from csv result and expected outputs
             try:
                 result = np.genfromtxt(os.path.join(student_dir, t_res[i]), delimiter=",", dtype=float, encoding='ISO-8859-1')
             except Exception as err:
-                print(f"{R}Error finding program's output file: {os.path.join(student_dir, t_res[i])}")
-                print(f"{O}{err}{W}")
+                print(colored(f"Error finding program's output file: {os.path.join(student_dir, t_res[i])}", "red"))
+                print(colored(f"{err}", "orange"))
                 continue
 
             try:
                 expected = np.genfromtxt(t_output[i], delimiter=",", dtype=float, encoding='ISO-8859-1')
             except Exception as err:
-                print(f"{R}Error finding the expected output file: ")
-                print(f"{O}{err}{W}")
+                print(colored(f"Error finding the expected output file: ", "red"))
+                print(colored(f"{err}", "orange"))
                 continue
 
             # compare file dims
             matches = False
             if False:
-                print(f"{R}Output file {t_output[i]}'s dimensions are different from expected result's!{W}")
+                print(colored(f"Output file {t_output[i]}'s dimensions are different from expected result's!", "red"))
                 continue
 
             # compare the files by simply looking at the text
             elif exact:
                 if DEBUG:
-                    print(f"{O}Testing exact values...{W}")
+                    print(colored(f"Testing exact values...", "orange"))
                 matches = filecmp.cmp(
                     t_output[i],
                     os.path.join(student_dir, t_res[i]),
@@ -126,15 +119,15 @@ def grade_problem(student_dir, t_output, t_res, commands, exact):
             # compare by considering value-errors
             else:
                 if DEBUG:
-                    print(f"{O}Testing approximate values...{W}")
+                    print(colored(f"Testing approximate values...", "orange"))
                 diff = np.sum(np.absolute(expected - result))
                 diff = diff / np.ravel(expected).shape[0]
-                print(f"{R}DIFF: {diff}{W}")
+                print(colored(f"DIFF: {diff}", "red"))
                 if diff < 0.1:
                     matches = True
 
             if not matches:
-                print(f"{R}TRY AGAIN{W}")
+                print(colored(f"TRY AGAIN", "red"))
                 # if there is an error running the command,
                 #   try placing the num_threads at the end
                 command = f"(cd {student_dir} && ./{commands[i][0]}"
@@ -149,35 +142,35 @@ def grade_problem(student_dir, t_output, t_res, commands, exact):
 
                 # skip over if cant run the program...
                 if err != 0:
-                    print(f"{R}Error running the program {commands[i][0]}{W}")
+                    print(colored(f"Error running the program {commands[i][0]}", "red"))
                     continue
 
                 # get data from csv result and expected outputs
                 try:
                     result = np.genfromtxt(os.path.join(student_dir, t_res[i]), delimiter=",", dtype=float, encoding='ISO-8859-1')
                 except Exception as err:
-                    print(f"{R}Error finding program's output file: {os.path.join(student_dir, t_res[i])}")
-                    print(f"{O}{err}{W}")
+                    print(colored(f"Error finding program's output file: {os.path.join(student_dir, t_res[i])}", "red"))
+                    print(colored(f"{err}", "orange"))
                     continue
 
                 try:
                     expected = np.genfromtxt(t_output[i], delimiter=",", dtype=float, encoding='ISO-8859-1')
                 except Exception as err:
-                    print(f"{R}Error finding the expected output file: {t_output[i]}")
-                    print(f"{O}{err}{W}")
+                    print(colored(f"Error finding the expected output file: {t_output[i]}", "red"))
+                    print(colored(f"{err}", "orange"))
                     continue
 
                 # compare file dims
                 matches = False
                 if False: #expected.shape != result.shape:
-                    print(f"{R}Output file {t_output[i]}'s dimensions are different from expected result's!{W}")
+                    print(colored(f"Output file {t_output[i]}'s dimensions are different from expected result's!", "red"))
                     continue
 
 
                 # compare the files by simply looking at the text
                 elif exact:
                     if DEBUG:
-                        print(f"{O}Testing exact values...{W}")
+                        print(colored(f"Testing exact values...", "orange"))
                     matches = filecmp.cmp(
                         t_output[i],
                         os.path.join(student_dir, t_res[i]),
@@ -187,10 +180,10 @@ def grade_problem(student_dir, t_output, t_res, commands, exact):
                 # compare by considering value-errors
                 else:
                     if DEBUG:
-                        print(f"{O}Testing approximate values...{W}")
+                        print(colored(f"Testing approximate values...", "orange"))
                     diff = np.sum(np.absolute(expected - result))
                     diff = diff / np.ravel(expected).shape[0]
-                    print(f"{R}DIFF: {diff}{W}")
+                    print(colored(f"DIFF: {diff}", "red"))
                     if diff < 0.1:
                         matches = True
 
@@ -200,23 +193,23 @@ def grade_problem(student_dir, t_output, t_res, commands, exact):
                 scores[i] = 1
             else:
                 if DEBUG:
-                    print(f"{R}The expected output: {t_output[i]} does not match the result!{W}")
+                    print(colored(f"The expected output: {t_output[i]} does not match the result!", "red"))
 
             try:
                 t = np.genfromtxt(os.path.join(student_dir, commands[i][-1]), delimiter=',')
             except Exception as err:
-                print(f"{R}Error finding program's time file: {commands[i][-1]}{W}")
+                print(colored(f"Error finding program's time file: {commands[i][-1]}", "red"))
                 continue
             times[i] = t
 
             if DEBUG:
-                print(f"{Y}    Test result {i} = {scores[i]}")
-                print(f"{Y}    Time result {i} = {times[i]}s\n{W}")
+                print(colored(f"    Test result {i} = {scores[i]}", "yellow"))
+                print(colored(f"    Time result {i} = {times[i]}s\n", "yellow"))
 
     # catch the weird stuff
     except Exception as err:
-        print(f"\n{R}Unexpected error!")
-        print(f"{Y}{err}{W}")
+        print(colored(f"\nUnexpected error!", "red"))
+        print(colored(f"{err}", "yellow"))
 
     return [scores, times]
 
@@ -232,8 +225,8 @@ def autograde(in_this_dir, in_test_dir, test_names, in_student_name, grade_func)
 
     # Print the test dir and project dir
     if DEBUG:
-        print(f"{G} --> Test dir: {test_dir} {W}")
-        print(f"{G} --> Project dir: {this_dir} {W}")
+        print(colored(f" --> Test dir: {test_dir}", "green"))
+        print(colored(f" --> Project dir: {this_dir}", "green"))
 
     # student grade
     grade = pd.DataFrame(
