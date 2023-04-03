685dd49c2b0906d0e95e0348eb18092a0267a04f
Parker Brandt <parker.brandt101@gmail.com>
2023-04-03 14:38:41 -0500

    Initial commit for Project 1 autograder

---
+++ Project_1/YourSolution_Project_1/autograder_project_1.py
@@ -0,0 +1,54 @@
+import numpy as np
+import os
+import pandas as pd
+import sys
+
+# Tell the script where to find the base autograder
+sys.path.append("..")
+sys.path.append(os.path.join("..", ".."))
+from autograder_base import Base_Autograder
+
+# Colors
+W = '\033[0m'  # white (normal)
+R = '\033[31m'  # red
+O = '\033[33m'  # orange
+Y = '\033[93m'  # yellow
+G = '\033[32m'  # green
+
+
+class Autograder_1_2(Base_Autograder):
+
+    def __init__(self, in_student_name="student", in_this_dir=".", in_test_files=["..", "test_data"]):
+        super().__init__()
+
+        # Student information
+        self.student_name = in_student_name
+        self.is_grad = True
+        self.DEBUG = True
+
+        # Directory information
+        self.this_dir =         in_this_dir
+        self.student_files =    "Problem_2"
+        self.test_files =       ""
+
+        for i in range(len(in_test_files)):
+            self.test_files = os.path.join(self.test_files, in_test_files[i])
+    
+        self.test_files = os.path.join(self.test_files, "Problem_2")
+
+        # Test information
+        self.test_names = [
+            "P2-1",
+            "P2-2",
+            "P2-3"
+        ]
+
+
+    def autograde(self):
+        return
+    
+
+if __name__ == "__main__":
+
+    p1 = Autograder_1_2()
+
