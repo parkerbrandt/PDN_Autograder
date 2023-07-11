69d9645a4632b6774ce170e523af233c649dc6e3
Parker Brandt <parker.brandt101@gmail.com>
2023-07-11 11:26:01 -0500

    Filled out autograde() with prototype code

--- Project_2/YourSolution_Project_2/autograder_problem_2_2a.py
+++ Project_2/YourSolution_Project_2/autograder_problem_2_2a.py
@@ -21,7 +21,7 @@ Project 2, Problem 2a Autograder
 
 Autogrades a single student's code
 """
-class Autograder_2_2(Base_Autograder):
+class Autograder_2_2a(Base_Autograder):
 
     """
     Initializes variables
@@ -48,30 +48,177 @@ class Autograder_2_2(Base_Autograder):
         self.test_out_files =   os.path.join(self.test_out_files, "Problem_2")
 
         # Test information
-        self.threads = [2, 4, 8]
+        self.threads = [1, 2, 4, 8]
         self.test_names = [
-            ""
+            "P2a-T1"
         ]
 
-        def autograde(self):
-            return
+
+    """
+    Check if the student's answer is within a reasonable bound of the actual answer
+    Error Bound:
+        - Check that student's answer is within 1% of actual answer
+
+    Parameters:
+        - expected  (ndarray):  The actual answer read from test_data/
+        - result    (ndarray):  The student's answer
+    """
+    def is_error_within_bound(self, expected, result):
+
+        try:
+            # Make sure the shapes of the 
+            if expected.shape != result.shape:
+                raise Exception("Shapes of expected output and student output do not match")
+            
+            # Compare the two arrays
+            return np.array_equal(expected, result, equal_nan=True)
+
+        except Exception as err:
+            print(f"{R}Error reading output file:{W}")
+            print(f"{R}\t{err}{W}")
+
+        return
+
+
+    """
+    Autogrades Problem 2a
+    Overrides Base_Autograder.autograde()
+
+    Constructs a test by retrieving data about paths and data locations, then calls Base_Autograder.grade_problem()
+    to test and grade the problem
+    """
+    def autograde(self):
+        this_dir =      os.path.abspath(self.this_dir)
+        test_in_dir =   os.path.abspath(self.test_in_files)
+        test_out_dir =  os.path.abspath(self.test_out_files)
+
+        # Print the test dir and project dir
+        if self.DEBUG:
+            print(f"{G} --> Test dir: {test_in_dir}{W}")
+            print(f"{G} --> Project dir: {this_dir}{W}")
+
+        # get num cols for threads
+        columns = []
+        for t in self.threads:
+            for p in self.test_names:
+                columns.append(f"{p}-{t}th")
+
+        # student grades
+        grade = pd.DataFrame(
+            np.nan,
+            index=[self.student_name],
+            columns=columns
+        )
+
+        # student timing
+        time = pd.DataFrame(
+            np.nan,
+            index=[self.student_name],
+            columns=columns
+        )
+
+        # Input matrix files
+        t_mats_a = [
+            os.path.join(test_in_dir, "test1_input_mat_a.csv"),
+            os.path.join(test_in_dir, "test2_input_mat_a.csv"),
+            os.path.join(test_in_dir, "test3_input_mat_a.csv"),
+            os.path.join(test_in_dir, "test4_input_mat_a.csv")
+        ]
+
+        t_mats_b = [
+            os.path.join(test_in_dir, "test1_input_mat_b.csv"),
+            os.path.join(test_in_dir, "test2_input_mat_b.csv"),
+            os.path.join(test_in_dir, "test3_input_mat_b.csv"),
+            os.path.join(test_in_dir, "test4_input_mat_b.csv")      
+        ]
+
+        # Expected output files
+        t_out = [
+            os.path.join(test_out_dir, "test1_output_mat.csv"),
+            os.path.join(test_out_dir, "test2_output_mat.csv"),
+            os.path.join(test_out_dir, "test3_output_mat.csv"),
+            os.path.join(test_out_dir, "test4_output_mat.csv")
+        ]
+
+        # The actual output from the student
+        t_dir = os.path.join(this_dir, self.student_files)
+        t_get = []
+        t_tim = []
+
+        for out in range(len(t_out)):
+            t_get.append(os.path.join(t_dir, f"test{out}_output_mat.csv"))
+            t_tim.append(os.path.join(t_dir, f"test{out}_time.csv"))
+
+        for out in range(len(self.test_names)):
+            for i in range(len(self.threads)):
+                t_get[out].append(os.path.join(t_dir, f"result_{self.threads[i]}p_{out}.csv"))
+                t_tim[out].append(os.path.join(t_dir, f"time_{self.threads[i]}p_{out}.csv"))
+
+        # Generate commands for the program
+        # Command structure:
+        #       parallel_mult_max file_1.csv n_row_1 n_col_1 file_2.csv n_row_2 n_col_2 result.csv time.csv num_threads
+        test_data = [
+
+        ]
+        c_p2a = []
+
+        for file in range(len(self.test_names)):
+            c_p2a.append([
+
+            ])
+
+        # Command references
+        c_p2a_ref = {"r": 7, "t": 8}
+
+        # Autograde with test parameters
+        test_params = []
+
+        for file in range(len(self.test_names)):
+            test_params.append(
+                [t_dir, t_out[file], t_get[file], c_p2a[file], False, self.is_error_within_bound]
+            )
+
+        test_results = [None] * len(columns)
+
+        # Test every problem
+        grade_index = 0
+        for file in range(len(self.test_names)):
+            params = test_params[file]
+            result = self.grade_problem(
+                params[0],                      # student directory
+                [params[1]],                    # test output
+                [params[2]],                    # test results
+                [params[3]],                    # commands
+                c_p2a_ref,                      # command references
+                params[4],                      # exact
+                params[5]                       # error function to be passed
+            )
+
+            test_results[grade_index] = result[0]
+
+            # Add results to dataframes
+            grade.loc[self.student_name, columns[grade_index]] = test_results[grade_index][0]
+            grade_index += 1
+
+
+        return [grade, time]
         
 
 def main():
     print(f"{G}Autograding for Project 2 Problem 2:\n{W}")
     
-    p2 = Autograder_2_2()
+    p2 = Autograder_2_2a()
     res = p2.autograde()
 
     total   = len(res[0].columns)
     correct = int(res[0].sum(axis=1)[0])
 
     print(f"{Y}\nFinal Grades:{W}")
-    res[0].to_csv("P2_2_grades.csv")
+    res[0].to_csv("P2_2a_grades.csv")
     print(res[0])
 
     print(f"{Y}\nFinal Timings:{W}")
-    res[1].to_csv("P2_2_times.csv")
+    res[1].to_csv("P2_2a_times.csv")
     print(res[1])
 
     print((f"\n --> {correct}/{total} problems correct\n"))
